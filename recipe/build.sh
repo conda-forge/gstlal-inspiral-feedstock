fe2be8986a7e70a04c6206db751e3ccde4a082e0
Duncan Macleod <duncan.macleod@ligo.org>
2021-02-03 14:18:10 +0000

    update recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,9 +5,6 @@ set -ex
 mkdir -p _build
 pushd _build
 
-# conda-forge/conda-forge.github.io#621
-#find ${PREFIX} -name "*.la" -delete
-
 # only link libraries we actually use
 export GSL_LIBS="-L${PREFIX}/lib -lgsl"
 export GSTLAL_LIBS="-L${PREFIX}/lib -lgstlal -lgstlaltags -lgstlaltypes"
@@ -25,7 +22,6 @@ ${SRC_DIR}/configure \
 ;
 
 # build
-export CPU_COUNT="1"
 make -j ${CPU_COUNT} V=1 VERBOSE=1
 
 # install
